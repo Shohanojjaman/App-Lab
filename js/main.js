a676e19e56a52b70be745a67256cbfa191fe4352
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2022-05-16 16:53:02 +0600

    modified main.js for brand area.
    added slick slider function on main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 (function($) {
 "use strict";
     
+    $('.brand-wrepper').slick({
+        autoplay:true,
+        arrows:false,
+        slidesToShow: 5,
+        autoplaySpeed:2000
+    });
+
 })(jQuery)
