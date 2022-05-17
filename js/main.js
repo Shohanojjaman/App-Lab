a818da41c1e2081bfe3cc5ecc9cd13e2114eee84
Shohanojjaman Emon <shohanojjamanemon@gmail.com>
2022-05-17 18:34:28 +0600

    added some code for testimonial section slide.

--- js/main.js
+++ js/main.js
@@ -8,4 +8,13 @@
         autoplaySpeed:2000
     });
 
+    $('.testimonial-wrepper').slick({
+        autoplay:true,
+        arrows:true,
+        nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-arrow-right"></i></button>',
+        prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-arrow-left"></i></button>',
+        slidesToShow: 1,
+        autoplaySpeed:2000
+    });
+
 })(jQuery)
